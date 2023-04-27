7b1de09cd2dd43e0167064902d29640902a7ed3d
aliriodi <aliriodi@gmail.com>
2023-04-27 11:06:00 -0300

    se crea prueba 2 con la estructura de index y datos de la bd

---
+++ pages/prueba2.tsx
@@ -0,0 +1,38 @@
+import React from "react"
+import { GetStaticProps } from "next"
+import Layout from "../components/Layout"
+import Post, { PostProps } from "../components/Post"
+import { PrismaClient } from '@prisma/client'
+
+export const getStaticProps: GetStaticProps = async () =>{
+  const prisma = new PrismaClient()
+  const feed = await prisma.player.findMany();
+    //console.log('feed son objetos dentro de array con length= ' +  feed.length);
+  return {
+    
+    props:  JSON.parse(JSON.stringify({feed})) ,
+  };
+};
+
+type Props = {
+  feed: PostProps[];
+};
+
+const Blog: React.FC<Props> = (props) => {
+  return (
+    <>
+      <div className="page">
+        <h1>Creado por Alirio</h1>
+        <main>
+          {props.feed.map((post) => (
+            <div key={post.idPlayer} className="post">
+              <span>{post.idPlayer+"  "+post.firstname+" "+" "+post.lastname+" "+post.createdAt}</span>
+           </div>
+          ))}
+        </main>
+      </div>
+     
+   </>
+  );
+};
+export default Blog

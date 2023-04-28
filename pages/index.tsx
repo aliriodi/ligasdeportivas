3dccff6636ffb19d363c7bcad6407500e795713d
aliriodi <aliriodi@gmail.com>
2023-04-28 19:07:55 -0300

    metricas en index y prueba2

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,6 +7,23 @@ import { PrismaClient } from '@prisma/client'
 export const getStaticProps: GetStaticProps = async () =>{
   const prisma = new PrismaClient()
   const feed = await prisma.user.findMany();
+
+  //
+  await fetch('https://graph.netlify.com/graphql?app_id=b83d3c66-7e5b-47e6-96b4-5ef3cc6f57b6&show_metrics=true',
+  {
+    method: 'POST',
+    body: JSON.stringify({
+      query: `query HelloNetlifyGraph {
+    helloGraph(sessionId: "b4ee97b4-ccce-4772-9fb2-70a3be57cf2b") {
+      welcome
+      about
+      whatToDoNext
+    }
+  }`,
+    }),
+  }
+)
+.then((r) => r.json())
     //console.log('feed son objetos dentro de array con length= ' +  feed.length);
   return {
     
--- pages/prueba2.tsx
+++ pages/prueba2.tsx
@@ -10,6 +10,24 @@ export const getStaticProps: GetStaticProps = async () =>{
   const team = await prisma.team.findMany();
   const division = await prisma.division.findMany();
   
+
+  //metricas de netlify
+  await fetch('https://graph.netlify.com/graphql?app_id=b83d3c66-7e5b-47e6-96b4-5ef3cc6f57b6&show_metrics=false',
+  {
+    method: 'POST',
+    body: JSON.stringify({
+      query: `query HelloNetlifyGraph {
+    helloGraph(sessionId: "b4ee97b4-ccce-4772-9fb2-70a3be57cf2b") {
+      welcome
+      about
+      whatToDoNext
+    }
+  }`,
+    }),
+  }
+)
+.then((r) => r.json())
+  //
     //console.log('feed son objetos dentro de array con length= ' +  feed.length);
   return {
     

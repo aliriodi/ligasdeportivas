18ef246e7024f53fd8aa6ecde69e4c90bb7ee992
aliriodi <aliriodi@gmail.com>
2023-05-05 12:07:19 -0300

    mejorando el home

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,26 @@
 import React from "react"
 import { useState } from 'react';
 import { GetStaticProps } from "next"
+import Image from 'next/image'
 import Nav from "../components/Nav"
 import Post, { PostProps } from "../components/Post"
+import logoLiga from "../images/LigaDarioSalazar.png"
 
 
-import { PrismaClient } from '@prisma/client'
+import { Prisma, PrismaClient } from '@prisma/client'
 
-export  async function getdataBD(idDivision: number, idTeam: number) {
+export async function getdataBD(idDivision: number, idTeam: number) {
   let prisma = new PrismaClient();
-  const league = await prisma.league.findMany();
-  const division = await prisma.division.findMany();
+  const league = await prisma.league.findMany().then(prisma.$disconnect);
+  const division = await prisma.division.findMany().then(prisma.$disconnect);
 
   if (idDivision) {
     const team = await prisma.team.findMany({
       where: {
         idDivision: idDivision,
       },
-      orderBy: { name:'asc', }
-    });
+      orderBy: { name: 'asc', }
+    }).then(prisma.$disconnect);
   }
 
   if (idTeam) {
@@ -26,8 +28,8 @@ export  async function getdataBD(idDivision: number, idTeam: number) {
       where: {
         idTeam: idTeam,
       },
-      
-    });
+
+    }).then(prisma.$disconnect);
     const tiporesult = await prisma.result.findMany();
     const valueresult = await prisma.resultPlayer.findMany({
       where: { idTeam: idTeam, },
@@ -39,24 +41,23 @@ export  async function getdataBD(idDivision: number, idTeam: number) {
 
 
 export const getStaticProps: GetStaticProps = async () => {
-    //Funcion para cargar usuarios controlados de la BD sin saturar 
+  //Funcion para cargar usuarios controlados de la BD sin saturar 
   //la busqueda
-      
 
+  let prisma: PrismaClient;
   //Funcion para buscar de la BD los elementos de los Select 
-  const prisma = new PrismaClient()
+  if (prisma !== null) { prisma = new PrismaClient() }
   const league = await prisma.league.findMany();
   const division = await prisma.division.findMany();
-
   const team = await prisma.team.findMany({
     where: {
-     // idDivision: 1,
+      // idDivision: 1,
     },
-    orderBy: { name:'asc', }
+    orderBy: { name: 'asc', }
   });
   const player = await prisma.player.findMany({
     where: {
-    //  idTeam: 2,
+      //  idTeam: 2,
     },
   });
 
@@ -65,7 +66,7 @@ export const getStaticProps: GetStaticProps = async () => {
     //where: { idTeam: 0, },
     orderBy: { idResult: 'asc', }
   });
-  
+
   //console.log('feed son objetos dentro de array con length= ' +  feed.length);
   return {
     props: JSON.parse(JSON.stringify({
@@ -75,8 +76,7 @@ export const getStaticProps: GetStaticProps = async () => {
       league,
       tiporesult,
       valueresult,
-      
-      })),
+    })),
   };
 };
 
@@ -91,82 +91,67 @@ type Props = {
 const Blog: React.FC<Props> = (props) => {
   const [IDTEAM, setIDTEAM] = useState(0);
   const [IDDIVISION, setIDDIVISION] = useState(0);
-   return (
+  return (
     <>
       <Nav {...props} />
       <div className="page">
-      <p></p><div className="center">
+        <p></p><div className="center">
+
           <h1 > <p >Liga Dario Salazar </p></h1></div>
+
         {/* <h1>Creado por Equipo desarrollador ARQUICOM AJ</h1> */}
         <p></p>
         {/* <div>Cantidad de jugadores cargados: {" " + props.player.length + " "} pertencientes a {" " + props.team.length} equipos</div> */}
         <p></p>
-        <main>
-        <div className="center">
-          {/* <p><button type="button"  onClick={()=>setIDTEAM(IDTEAM===50?0:IDTEAM+1)} className="btn btn-primary">Primary</button></p> */}
-          {/* {props.league.map(p => <span>{'Liga ' + p.idLeague + " " + p.name}</span>)} */}
-          {/* <p>{IDTEAM}{IDDIVISION}</p> */}
-          <form className="custom-form">
-          <select className="custom-select" id="input2" onChange={()=>setIDDIVISION(parseInt((document.getElementById("input2")as HTMLInputElement).value)) } >
-            <option key={0} value="0">Seleccione Division</option>
-            {props.division.map(
-              division => <option key={division.idDivision} value={division.idDivision}>{division.name}</option>
-            )}
-              
-          </select>
-           <>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</>                                        {/* JS = parseInt(document.getElementById("input1").value))  */}
-          <select className="custom-select" id="input1" onChange={()=>setIDTEAM(parseInt((document.getElementById("input1")as HTMLInputElement).value)) } >
-            <option key='0' value="0">Seleccione Equipo</option>
-            {props.team.map(
-              team0 => team0.idDivision===IDDIVISION?<option key={team0.idTeam} value={team0.idTeam}>{team0.name}</option>:null
-                       
-            )}
-            
-          </select>
-          </form>
+        <div className="flex-container">
+          <div className="flex-item">
+            <Image className="logoHome" src={logoLiga} />
           </div>
-         
-          {props.team.filter(team0 => team0.idTeam ===IDTEAM).map(
-            post => (
-              <div key={post.idTeam}>Equipo:  {post.name}
-                <p>Division: {props.division.filter(element => element.idDivision === post.idDivision)[0].name}</p>
-                <div>
-                  <table className="table table-hover">
-                    <tbody>
-                      {/* <!-- Aplicadas en las filas --> */}
-                      {/* Fila de tipo resultados */}
-                      <tr>
-                        <th>Nombre</th>
-                        {props.tiporesult.map(result => <th>{result.name}</th>)}
-                      </tr>
-                      {/* Fila de Jugador mapeado nombre y datos  */}
-                      {props.player.map((player) => (
-                        //Metodo de empezar a escribir una Fila por participante y en 1era coolumna su nombre
-                        player.idTeam === post.idTeam ? <tr className="active"><td>{player.firstname + "  " + player.lastname}</td>
-                          {/* Mapeando los idResult primero, luego a la tabla  */}
-                          {props.tiporesult.map(tiporesult => <td>{props.valueresult.filter(valueresult => valueresult.idPlayer === player.idPlayer)[tiporesult.idResult - 1] ?
+          <div className="flex-item-content">
+            <table className="table table-hover">
+              <tbody>
+                {/* <!-- Aplicadas en las filas --> */}
+                <tr>
+                  <th ></th>
+                  <th> </th>
+                </tr>
+                <tr>
+                  <td className="center">TIGRITOS</td>
+                  <td className="center">CAPITANES</td>
+                </tr>
+                <tr >
+                  <td className="center">MELLIZOS</td>
+                  <td className="center">COCODRILOS</td>
+                </tr>
+                <tr >
+                  <td className="center">PIRATAS</td>
+                  <td className="center">GUARDIANES</td>
+                </tr>
+                <tr >
+                  <td className="center">ESCORPIONES</td>
+                  <td className="center">SABUESOS</td>
+                </tr>
+                <tr >
+                  <td className="center">GIGANTES</td>
+                  <td className="center">MVP</td>
+                </tr>
+                <tr >
+                  <td className="center">BRAVOS</td>
+                  <td className="center">ANGELES</td>
+                </tr>
+                <tr >
+                  <td className="center">NEW ASTROS</td>
+                  <td className="center">JUVENCIO</td>
+                </tr>
+                {/* <!-- Aplicadas en las celdas (<td> o <th>) --> */}
+              </tbody>
+            </table>
+          </div>
 
-                            props.valueresult.filter(valueresult => valueresult.idPlayer === player.idPlayer)[tiporesult.idResult - 1].value : null}</td>)}</tr> : null))}
-                      {/* //metodo para ordenar el vector por id y se imprima bien */}
-                      {/* sort((x, y) =>x.idResult >y.idResult?1:-1). */}
-                      {/* <!-- Aplicadas en las celdas (<td> o <th>) --> */}
-                    </tbody>
-                  </table>
-                </div>
-
-                {/* {props.player.map((post2) => (
-                  post2.idTeam === post.idTeam ?
-                    <div key={post2.idPlayer} className="post">
-                      <span>{post2.idPlayer + "  " + post2.firstname + " " + " " + post2.lastname + " "}</span>
-                      <span>{post2.idPlayer}</span>
-                    </div>
-                    : null
-                ))}    <p></p> */}
-
-              </div>
-            )
-          )}
-        </main>
+          <div className="flex-item">
+            <Image className="logoHome" src={logoLiga} />
+          </div>
+        </div>
       </div>
     </>
   );

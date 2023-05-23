2964583acbabac00209d28a1d0d3f4b732c1e81f
aliriodi <aliriodi@gmail.com>
2023-05-23 19:05:15 -0300

    analytics instaladas ern _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,13 @@
 import { AppProps } from "next/app";
+import { Analytics } from '@vercel/analytics/react';
 import './style.css';
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
+    <>
     <Component {...pageProps} />
+    <Analytics />
+    </>
   );
 };
 
